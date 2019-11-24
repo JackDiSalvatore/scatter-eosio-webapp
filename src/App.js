55be8230cdefaeee478b5ddd1a4c709b95f90907
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-11-24 14:27:00 -0600

    re-adding auto login with scatter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 import Home from './pages/home'
+import {attemptAutoLogin} from './scatter/scatter_actions'
 
 class App extends Component{
-    // componentDidMount() {
-    //     this.props.dispatch(attemptAutoLogin());
-    // }
+    componentDidMount() {
+        this.props.dispatch(attemptAutoLogin());
+    }
 
     render(){
         return (
